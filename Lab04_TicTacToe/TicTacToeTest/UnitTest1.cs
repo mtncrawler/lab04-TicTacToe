3e8ab4bb22b8776f1e25d5c73331c12ac3d07673
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-18 17:52:18 -0700

    starting tests

--- Lab04_TicTacToe/TicTacToeTest/UnitTest1.cs
+++ Lab04_TicTacToe/TicTacToeTest/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using Lab04_TicTacToe.Classes;
 
 namespace TicTacToeTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void WinnerWinnerChickenDinner()
         {
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game game = new Game(p1, p2);
+            Board board = new Board();
 
+            board.GameBoard = new string[,]
+            {
+            {"1", "X", "3"},
+            {"4", "X", "6"},
+            {"7", "X", "9"},
+            };
+
+            Assert.True(game.CheckForWinner(game.Board));
         }
     }
 }
