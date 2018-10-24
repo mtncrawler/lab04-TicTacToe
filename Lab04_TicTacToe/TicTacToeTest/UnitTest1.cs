10d542ec026f7e47bf977f8f3fb16e3b168e5384
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-23 22:36:35 -0700

    passed tests for winning combos

--- Lab04_TicTacToe/TicTacToeTest/UnitTest1.cs
+++ Lab04_TicTacToe/TicTacToeTest/UnitTest1.cs
@@ -6,20 +6,68 @@ namespace TicTacToeTest
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// test for horizontal winner
+        /// </summary>
         [Fact]
-        public void WinnerWinnerChickenDinner()
+        public void HorizontalWinner()
+        {
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game game = new Game(p1, p2);
+            Board board = new Board();
+            
+            Position mark1 = new Position(0, 0);
+            Position mark2 = new Position(0, 1);
+            Position mark3 = new Position(0, 2);
+
+            game.Board.GameBoard[mark1.Row, mark1.Column] = "X";
+            game.Board.GameBoard[mark2.Row, mark2.Column] = "X";
+            game.Board.GameBoard[mark3.Row, mark3.Column] = "X";
+
+            Assert.True(game.CheckForWinner(game.Board));
+        }
+
+        /// <summary>
+        /// test for vertical winner
+        /// </summary>
+        [Fact]
+        public void VerticalWinner()
         {
             Player p1 = new Player();
             Player p2 = new Player();
             Game game = new Game(p1, p2);
             Board board = new Board();
 
-            board.GameBoard = new string[,]
-            {
-            {"1", "X", "3"},
-            {"4", "X", "6"},
-            {"7", "X", "9"},
-            };
+            Position mark1 = new Position(0, 0);
+            Position mark2 = new Position(1, 0);
+            Position mark3 = new Position(2, 0);
+
+            game.Board.GameBoard[mark1.Row, mark1.Column] = "X";
+            game.Board.GameBoard[mark2.Row, mark2.Column] = "X";
+            game.Board.GameBoard[mark3.Row, mark3.Column] = "X";
+
+            Assert.True(game.CheckForWinner(game.Board));
+        }
+
+        /// <summary>
+        /// test for diagonal winner
+        /// </summary>
+        [Fact]
+        public void DiagonalWinner()
+        {
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game game = new Game(p1, p2);
+            Board board = new Board();
+
+            Position mark1 = new Position(0, 0);
+            Position mark2 = new Position(1, 1);
+            Position mark3 = new Position(2, 2);
+
+            game.Board.GameBoard[mark1.Row, mark1.Column] = "X";
+            game.Board.GameBoard[mark2.Row, mark2.Column] = "X";
+            game.Board.GameBoard[mark3.Row, mark3.Column] = "X";
 
             Assert.True(game.CheckForWinner(game.Board));
         }
